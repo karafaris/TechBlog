56a6ee307fd08442bdd33e7b3ccf5a32cef8ccff
Kara Faris <karafaris@icloud.com>
2023-07-21 07:51:40 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,19 @@
-let sequelize;
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
-}
+// Enable access to .env variables
+require('dotenv').config();
+
+// Use environment variables to connect to database
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
+
+module.exports = sequelize;
