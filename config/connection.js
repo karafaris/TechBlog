58db67243a69e80dec512ce145550da1c130cd85
Kara Faris <karafaris@icloud.com>
2023-07-23 13:12:36 -0500

    Updated

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ require('dotenv').config();
 
 // Use environment variables to connect to database
 const sequelize = new Sequelize(
+  process.env.DB_HOST,
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
     dialect: 'mysql',
     port: 3306
   }
